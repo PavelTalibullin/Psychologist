beaf22ebd5b04052114396bc8f7764e3f1bb678a
PavelTalibullin <pavlotalibullin@gmail.com>
2023-07-31 20:27:17 +0300

    .

--- js/scriptBurger.js
+++ js/scriptBurger.js
@@ -60,6 +60,10 @@ if (menuLinks.length > 0) {
 }
 
 // Бургер
+const nav = document.getElementById("header__nav");
+let contact = '<div class="d-flex header__contact"><a href="#"><img src="/images/icon/insta.svg" alt="instagram" class="header__contact_img"></a><a href="#"><img src="/images/icon/teleg.svg" alt="telegram" class="header__contact_img"></a><a href="#"><img src="/images/icon/whats.svg" alt="whatsapp" class="header__contact_img header__contact_img_ml"></a></div>';
+let burgerMenu = '<nav class="header__nav" id="header__nav"><ul class="d-flex header__nav_list"><li><a data-goto=".page__section_1" href="#" class="menu__link">Обо мне</a></li><li><a data-goto=".page__section_2" href="#" class="menu__link">С чем я работаю</a></li><li><a data-goto=".page__section_3" href="#" class="menu__link">Цены</a></li><li><a data-goto=".page__section_4" href="#" class="menu__link">Отзывы</a></li></ul></nav>';
+
 document.addEventListener("DOMContentLoaded", function () {
   document.getElementById("burger").addEventListener("click", function () {
     document.querySelector(".header").classList.toggle("open");
